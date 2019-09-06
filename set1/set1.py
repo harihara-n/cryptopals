9f190cd5d5014101086851084b6089c843670a89
Harihara Krishnan Narayanan <harihara@squareup.com>
2019-09-05 23:19:10 -0700

    Solution to problem 3.

--- set1/set1.py
+++ set1/set1.py
@@ -1,3 +1,5 @@
+#!/usr/local/bin/python3.7
+
 #
 # Cryptopals Set 1 - https://cryptopals.com/sets/1
 #
@@ -38,6 +40,27 @@ def xor_two_byte_strings(byte_string_1, byte_string_2):
 def xor_two_hex_strings(hex_string_1, hex_string_2):
   return byte_string_to_hex_string(xor_two_byte_strings(hex_string_to_byte_string(hex_string_1), hex_string_to_byte_string(hex_string_2)))
 
+def xor_byte_string_with_byte(byte_string, byte):
+  return_bytes = b''
+  for i in range(len(byte_string)):
+    return_bytes += (bytes([byte_string[i]^byte]))
+  return return_bytes
+
+def score_byte_string_on_english_characters_frequency(bytes_string):
+  character_frequencies = {
+    'a': .08167, 'b': .01492, 'c': .02782, 'd': .04253,
+    'e': .12702, 'f': .02228, 'g': .02015, 'h': .06094,
+    'i': .06094, 'j': .00153, 'k': .00772, 'l': .04025,
+    'm': .02406, 'n': .06749, 'o': .07507, 'p': .01929,
+    'q': .00095, 'r': .05987, 's': .06327, 't': .09056,
+    'u': .02758, 'v': .00978, 'w': .02360, 'x': .00150,
+    'y': .01974, 'z': .00074, ' ': .13000
+  }
+  score = 0
+  for byte in bytes_string:
+    score += character_frequencies.get(chr(byte), 0)
+  return score
+
 
 # Challenge 1: https://cryptopals.com/sets/1/challenges/1
 hex_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
@@ -47,3 +70,15 @@ print("Challenge 1: {0}".format(hex_string_to_b64_string(hex_string)))
 hex_string_1 = "1c0111001f010100061a024b53535009181c"
 hex_string_2 = "686974207468652062756c6c277320657965"
 print("Challenge 2: {0}".format(xor_two_hex_strings(hex_string_1, hex_string_2)))
+
+# Challenge 3: https://cryptopals.com/sets/1/challenges/3
+hex_string = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+answer_bytes_string = b''
+max_score = 0
+for byte in range(256):
+  bytes_string = xor_byte_string_with_byte(hex_string_to_byte_string(hex_string), byte)
+  score = score_byte_string_on_english_characters_frequency(bytes_string)
+  if max_score < score:
+    max_score = score
+    answer_bytes_string = bytes_string
+print("Challenge 3: {0}".format(answer_bytes_string))
