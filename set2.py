3657d1d41c36aaed12c1d4b67381938fa8d9be55
Harihara Krishnan Narayanan <harihara@squareup.com>
2019-09-22 00:16:02 -0700

    Add aes128_cbc_encrypt and generate_aes_key

--- set2.py
+++ set2.py
@@ -8,6 +8,7 @@ from Crypto.Cipher import AES
 import pdb
 import requests
 import set1
+import secrets
 
 def do_pkcs7_padding(byte_string, pad_length):
   return_byte_string = bytes(byte_string)
@@ -19,26 +20,44 @@ def do_pkcs7_padding(byte_string, pad_length):
 def aes128_cbc_decrypt(ciphertext, iv, key):
   aes = AES.new(key, AES.MODE_ECB)
   def decrypt_chunk(chunk, iv):
-    chunk = bytes(chunk)
     return set1.xor_two_byte_strings(aes.decrypt(chunk), iv)
 
   chunks = set1.split_into_n_sized_chunks(ciphertext, 16)
   decrypted_bytes = b''
   for i, chunk in enumerate(chunks):
     if i == 0:
-      decrypted_bytes += decrypt_chunk(chunk, iv)
+      decrypted_bytes += decrypt_chunk(bytes(chunk), iv)
     else:
-      decrypted_bytes += decrypt_chunk(chunk, chunks[i-1])
+      decrypted_bytes += decrypt_chunk(bytes(chunk), chunks[i-1])
   return decrypted_bytes
 
+def aes128_cbc_encrypt(text, iv, key):
+  aes = AES.new(key, AES.MODE_ECB)
+  def encrypt_chunk(chunk, iv):
+    return aes.encrypt(set1.xor_two_byte_strings(chunk, iv))
 
-# Challenge 9: https://cryptopals.com/sets/2/challenges/9
-string = "YELLOW SUBMARINE"
-print("Challenge 9: {0}".format(do_pkcs7_padding(string.encode('utf-8'), 20)))
+  chunks = set1.split_into_n_sized_chunks(text, 16)
+  encrypted_bytes = b''
+  for i, chunk in enumerate(chunks):
+    if i == 0:
+      prev_encrypted_bytes = encrypt_chunk(bytes(chunk), iv)
+    else:
+      prev_encrypted_bytes = encrypt_chunk(bytes(chunk), prev_encrypted_bytes)
+    encrypted_bytes += prev_encrypted_bytes
+  return encrypted_bytes
 
-# Challenge 10: https://cryptopals.com/sets/2/challenges/10
-b64_string = requests.get('https://cryptopals.com/static/challenge-data/10.txt').text.strip()
-byte_string = set1.b64_string_to_byte_string(b64_string)
-iv = bytes([0] * 16)
-key = b'YELLOW SUBMARINE'
-print("Challenge 10: {0}".format(aes128_cbc_decrypt(byte_string, iv, key)))
+def generate_aes_key(num_bytes):
+  return secrets.token_bytes(num_bytes)
+
+
+if __name__ == '__main__':
+  # Challenge 9: https://cryptopals.com/sets/2/challenges/9
+  string = "YELLOW SUBMARINE"
+  print("Challenge 9: {0}".format(do_pkcs7_padding(string.encode('utf-8'), 20)))
+
+  # Challenge 10: https://cryptopals.com/sets/2/challenges/10
+  b64_string = requests.get('https://cryptopals.com/static/challenge-data/10.txt').text.strip()
+  byte_string = set1.b64_string_to_byte_string(b64_string)
+  iv = bytes([0] * 16)
+  key = b'YELLOW SUBMARINE'
+  print("Challenge 10: {0}".format(aes128_cbc_decrypt(byte_string, iv, key)))
